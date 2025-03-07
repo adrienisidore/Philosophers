db4824b06aea4a16e353616f5428d83ba05f91c4
Adrien Isidore <aisidore@bess-f2r4s10.clusters.42paris.fr>
2025-03-07 12:36:42 +0100

    rendu

--- main.c
+++ main.c
@@ -6,13 +6,24 @@
 /*   By: aisidore <aisidore@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 15:53:12 by aisidore          #+#    #+#             */
-/*   Updated: 2025/03/06 20:10:37 by aisidore         ###   ########.fr       */
+/*   Updated: 2025/03/07 12:35:58 by aisidore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//Supp executable + les .o////////////////////////////////////////////////////////////
-
 #include "philo.h"
+
+static void	ft_wait(t_data *dt)
+{
+	t_philo	*curr;
+
+	pthread_join(dt->monit, NULL);
+	curr = dt->philos;
+	while (curr)
+	{
+		pthread_join(curr->thread, NULL);
+		curr = curr->next;
+	}
+}
 
 int	main(int ac, char **av)
 {
@@ -33,13 +44,7 @@ int	main(int ac, char **av)
 	}
 	if (pthread_create(&dt->monit, NULL, ft_monitor, dt))
 		return (ft_fail(dt, curr));
-	pthread_join(dt->monit, NULL);
-	curr = dt->philos;
-	while (curr)
-	{
-		pthread_join(curr->thread, NULL);
-		curr = curr->next;
-	}
+	ft_wait(dt);
 	pthread_mutex_destroy(&dt->mut_fail);
 	return (ft_freeall(dt->forks, dt->philos, dt, NULL));
 }
